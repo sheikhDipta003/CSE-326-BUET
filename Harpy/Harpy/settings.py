afac2b4fa5bf9a41c365934b12280a690b46d2ab
sheikhdipta <sheikhdipta@gmail.com>
2023-08-25 22:46:35 +0600

    django-proj(Harpy) : showing ModuleNotFoundError, so removed crispy and bootstrap-4

--- Harpy/Harpy/settings.py
+++ Harpy/Harpy/settings.py
@@ -34,7 +34,6 @@ INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
     'crispy_forms',
-    'crispy_bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
